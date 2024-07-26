263675cc04a9fdee11fdb90d9f2fdb4d2ee372b7
Alejandro Box <solideralejandro@gmail.com>
2024-07-26 23:34:01 +0200

    Add first exercise

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/SoliderAlejandroESP.js
@@ -0,0 +1,15 @@
+// https://ecma-international.org/
+/* https://ecma-international.org/ */
+
+let variable1 = "Hola Mundo!"
+const variable2 = "Adios Mundo!"
+
+const cadenaTexto = "Hola Mundo Cruel!"
+const entero = 123
+const enteroGrande = 123987123918273n
+const booleano = true
+const indefinido = undefined
+const simbolo = Symbol()
+const nulo = null
+
+console.log("¡Hola, JavaScript!")
